c5801f24a750f9c90723ef7f7e2a5877ca21a49c
Aldairz16 <aldairalexanderpa@gmail.com>
2026-01-16 23:23:21 -0500

    fix(build): add use client directive to history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useStore } from "@/lib/store";
 import { TransactionItem } from "@/components/transactions/transaction-item";
 import { TransactionTable } from "@/components/transactions/transaction-table";
